acb890b274c070b45e0d9cd8813d69b3b42d2b67
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-13 22:36:22 +0700

    Created RobotMap constructor

--- robotGame/RobotMap.java
+++ robotGame/RobotMap.java
@@ -1,5 +1,11 @@
 package robotGame;
 
 public class RobotMap {
-    
+    private final int n;
+    private final int m;
+
+    RobotMap(int n, int m) {
+        this.n = n;
+        this.m = m;
+    }
 }
