0173d19bfbfe67b7d9d05ced3dd46a3d0cc11f19
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-13 21:02:58 +0700

    Initial commit

---
+++ robotGame/EntryPoint.java
@@ -0,0 +1,10 @@
+package robotGame;
+
+/**
+ * main - entry point
+ */
+public class EntryPoint {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ robotGame/RobotMap.java
@@ -0,0 +1,5 @@
+package robotGame;
+
+public class RobotMap {
+    
+}
