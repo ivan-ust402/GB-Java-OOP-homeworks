e3a61f38ee1543736bdd6dd556cd6b485787da33
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-13 23:41:37 +0700

    Created validatePointIsCorrectand validatePointIsFree method

--- robotGame/RobotMap.java
+++ robotGame/RobotMap.java
@@ -1,17 +1,37 @@
 package robotGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RobotMap {
     private final int n;
     private final int m;
 
+    private final List<Robot> robots;
+
     RobotMap(int n, int m) {
         this.n = n;
         this.m = m;
+        this.robots = new ArrayList<>();
     }
 
     public Robot createRobot(Point point) {
         Robot robot = new Robot(point);
         return robot;
+    }
+
+    private void validatePointIsCorrect(Point point) {
+        if (point.x() < 0 || point.x() > n || point.y() < 0 || point.y() > m) {
+            throw new IllegalStateException("Некорректное значение точки!\n Точка находится за пределами игрового поля");
+        }
+    }
+
+    private void validatePointIsFree(Point point) {
+        for (Robot robot : robots) {
+            if (robot.point.equals(point)) {
+                throw new IllegalStateException("Точка " + point + " занята!");
+            }
+        }
     }
 
     public class Robot {
