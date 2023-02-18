200e30b4a49d0a8842da6be8c4878712a3ef0901
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-18 19:59:21 +0700

    Created RoadObstacleAdapter

--- homework2/competitionGame/adapters/RoadObstacleAdapter.java
+++ homework2/competitionGame/adapters/RoadObstacleAdapter.java
@@ -1,5 +1,19 @@
 package homework2.competitionGame.adapters;
 
-public class RoadObstacleAdapter {
+import homework2.competitionGame.interfaces.Obstacle;
+import homework2.competitionGame.interfaces.Participant;
+import homework2.competitionGame.obstacles.Road;
+
+public class RoadObstacleAdapter implements Obstacle{
+    private final Road road;
+    
+    public RoadObstacleAdapter(Road road) {
+        this.road = road;
+    }
+
+    @Override
+    public boolean pass(Participant participant) {
+        return road.pass(participant);
+    }
     
 }
