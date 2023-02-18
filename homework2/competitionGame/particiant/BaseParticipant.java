8f161c4aa96c7d67296d3a9e42a9b075a7821b3c
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-18 19:52:05 +0700

    Created BaseParticipant abstract class

--- homework2/competitionGame/particiant/BaseParticipant.java
+++ homework2/competitionGame/particiant/BaseParticipant.java
@@ -1,5 +1,37 @@
 package homework2.competitionGame.particiant;
 
-public class BaseParticipant {
-    
+import homework2.competitionGame.interfaces.Participant;
+
+public abstract class BaseParticipant implements Participant {
+    private final String name;
+    private final int jump;
+    private final int run;
+    private final int swim;
+
+    public BaseParticipant (String name, int jump, int run, int swim) {
+        this.name = name;
+        this.jump = jump;
+        this.run = run;
+        this.swim = swim;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getJump() {
+        return jump;
+    }
+
+    @Override
+    public int getRun() {
+        return run;
+    }
+
+    @Override
+    public int getSwim() {
+        return swim;
+    }
 }
