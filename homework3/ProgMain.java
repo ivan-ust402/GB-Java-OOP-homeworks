c83f704cae95b3e3c4e67f04de9a565605e62bc5
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-21 17:25:09 +0700

    homework3: initial commit

---
+++ homework3/ProgMain.java
@@ -0,0 +1,14 @@
+package homework3;
+
+/* Создать класс Notebook с полями (price(double), ram(int))
+1. Сгенерировать список объектов типа Notebook с разными значениям price и ram
+2. Релизовать 3 сортировки: 1 - по цене, 2 - по памяти, 3 - сначала по памяти, потом по цене
+3. Отсортировать тремя способами стандартными средствами (Collections#sort или List#sort)
+
+Повторить про Iterator и Iterable */
+
+public class ProgMain {
+    public static void main(String[] args) {
+        
+    }
+}
