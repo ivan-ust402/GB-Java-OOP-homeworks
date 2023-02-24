6e8e750744cf5e7344e28293bfcbcef1fdcfa339
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-24 18:13:05 +0700

    Homework 4: Initial commit

---
+++ homework4/Apple.java
@@ -0,0 +1,9 @@
+package homework4;
+
+public class Apple extends Fruit {
+
+    public Apple(double weight) {
+        super(weight);
+    }
+
+}
---
+++ homework4/GoldenApple.java
@@ -0,0 +1,9 @@
+package homework4;
+
+public class GoldenApple extends Apple {
+
+    public GoldenApple(double weight) {
+        super(weight);
+    }
+
+}
