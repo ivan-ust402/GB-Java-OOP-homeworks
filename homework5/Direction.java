6a886683a70f2786d1a90c410b7b06dafc96e3d9
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-27 23:58:14 +0700

    Created ofString method

--- homework5/Direction.java
+++ homework5/Direction.java
@@ -1,8 +1,19 @@
 package homework5;
 
+import java.util.Optional;
+
 /*
  * Перечисление направления движений робота
  */
 public enum Direction {
-    TOP, RIGHT, BOTTOM, LEFT
+    TOP, RIGHT, BOTTOM, LEFT;
+
+    public static Optional<Direction> ofString (String str) {
+        for (Direction value : values()) {
+            if (str.equals(value.name())){
+                return Optional.of(value);
+            }
+        }
+        return Optional.empty();
+    }
 }
