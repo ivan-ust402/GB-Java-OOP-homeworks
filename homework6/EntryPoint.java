be6f043180b5f1985a32cdb90714b21f62c137c1
Ivan Ustyantsev <vangold915@gmail.com>
2023-03-04 23:00:14 +0700

    Homework6: Initial commit

---
+++ homework6/EntryPoint.java
@@ -0,0 +1,187 @@
+package homework6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import homework6.RobotMap.Robot;
+
+// HW:
+// 1. В проекте RobotMap сделать интерфейс для робота. Везде наружу (в метод #main) отдавать интерфейс. Создание объекта конкретного класса должно быть ровно в одном месте внутри класса RobotMap.
+// 2. * При желании, сделать рефакторинг проекта RobotMap, описать проделанную работу и объяснить, какую пользу несут изменения.
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        // VIEW
+        Scanner sc = new Scanner(System.in, "Cp866");
+        System.out.println("Приветствуем в игре RobotGame!");
+        System.out.println("Введите размеры карты: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // Читаем строку до конца
+        sc.nextLine();
+        RobotMap map = new RobotMap(n, m);
+        System.out.println("Карта успешно создана!");
+        final CommandManager manager = new CommandManager(map);
+
+        while (true) {
+            System.out.println("""
+                Список доступных действий:
+                    1. Для создания робота введите create x, y, где x, y - 
+                    координаты местоположения нового робота.
+                    2. Для вывода списка всех созданных роботов, введите list 
+                    3. Для перемещения робота введите move id x, y, где id -
+                    идентификатор робота
+                    4. Для изменения направления введите changedir id 
+                    DIRECTION, где id - идентификатор робота, DIRECTION -
+                    одно из значений (TOP, RIGHT, BOTTOM, LEFT) 
+                    5. Для удаления робота введите delete id, где id - 
+                    идентификатор робота 
+                    6. Для выхода напишите exit
+                    ...список будет пополняться
+                    """); 
+            String command = sc.nextLine();
+            // Инкапсулируем обработку команд
+            manager.acceptCommand(command);
+        }       
+    }
+    // CONTROLLER
+    private static class CommandManager {
+        RobotMap map;
+        private final List<CommandHandler> handlers;
+
+        CommandManager(RobotMap map) {
+            this.map = map;
+            handlers = new ArrayList<>();
+            initHandlers();
+        }
+
+        private interface CommandHandler{
+            String name();
+            void runCommand(String[] args);
+        }
+
+        private void initHandlers() {
+            initCreateCommandHandler();
+            initListCommandHandler();
+            initMoveCommandHandler();
+            initChangeDirCommandHandler();
+            initDeleteCommandHandler();
+            initExitCommandHandler();
+        }
+
+        private void initCreateCommandHandler() {
+            handlers.add(createHandler("create", args -> {
+                int x = Integer.parseInt(args[0]);
+                int y = Integer.parseInt(args[1]);
+                RobotMap.Robot robot = map.createRobot(new Point(x,y));
+                System.out.println("Робот " + robot + " успешно создан!");
+            }));
+        }
+
+        private void initListCommandHandler() {
+            handlers.add(createHandler("list", args -> {
+                map.acceptRobots(System.out::println);
+            }));
+        }
+
+        private void initMoveCommandHandler() {
+            handlers.add(createHandler("move", args -> {
+                Long robotId = Long.parseLong(args[0]);
+                Optional<RobotMap.Robot> robot = map.getByID(robotId);
+                robot.ifPresentOrElse(RobotMap.Robot::move, () -> {
+                    System.out.println("Робот с идентификатором " + robotId + " не найден!");
+                });
+            }));
+        }
+
+        private void initChangeDirCommandHandler() {
+            handlers.add(createHandler("changedir", args -> {
+                Long robotId = Long.parseLong(args[0]);
+                String dirInput = args[1];
+                Optional<Direction> dirOptional = Direction.ofString(dirInput);
+                Optional<RobotMap.Robot> robot = map.getByID(robotId);
+                if(dirOptional.isEmpty()) {
+                    System.out.println("Направления движения " + dirInput + " не существует!");
+                }
+                if(robot.isEmpty()) {
+                    System.out.println("Робот с идентификатором " + robotId + " не найден!");
+                } 
+                if(dirOptional.isPresent() && robot.isPresent()) {
+                    robot.get().changeDirection(dirOptional.get());
+                }            
+            }));
+        }
+
+        private void initDeleteCommandHandler() {
+            handlers.add(createHandler("delete", args -> {
+                Long robotId = Long.parseLong(args[0]);
+                Optional<RobotMap.Robot> robot = map.getByID(robotId);
+                robot.ifPresentOrElse(new Consumer<RobotMap.Robot>() {
+
+                    @Override
+                    public void accept(Robot robot) {
+                        map.deleteRobot(robot);
+                        System.out.println();
+                    }
+
+                    }, () -> {
+                        System.out.println("Робот с идентификатором " + robotId + " не найден!");
+                    }
+                );
+            }));
+        }
+
+        private void initExitCommandHandler() {
+            handlers.add(createHandler("exit", args -> {
+                System.exit(0);
+            }));
+        }
+
+        private CommandHandler createHandler(String name, Consumer<String[]> delegate) {
+            return new CommandHandler() {
+
+                @Override
+                public String name() {
+                    return name;
+                }
+
+                @Override
+                public void runCommand(String[] args) {
+                    delegate.accept(args);
+                }
+                
+            };
+            
+        }
+
+
+
+        public void acceptCommand(String command) {
+            String[] split = command.split(" ");
+            String commandName = split[0];
+            String[] commandArgs = Arrays.copyOfRange(split, 1, split.length);
+            boolean found = false;
+
+            for (CommandHandler handler : handlers) {
+                if(commandName.equals(handler.name())) {
+                    found = true;
+
+                    try {
+                        handler.runCommand(commandArgs);
+                    } catch (Exception e) {
+                        System.err.println("Во время обработки команды \"" + commandName + "\" произошла ошибка: " + e.getMessage());
+                    }
+                    
+                }
+            }
+
+            if(!found) {
+                System.out.println("Команда не найдена!");
+            }
+        }
+    }
+}
---
+++ homework6/RobotMap.java
@@ -0,0 +1,199 @@
+package homework6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class RobotMap {
+    private final int n;
+    private final int m;
+    private final int robotCount;
+
+    private final List<Robot> robots;
+
+    /*
+     * Конструктор класса RobotMap
+     */
+    public RobotMap(int n, int m, int robotCount) {
+        validateRobotMapBoundaries(n, m);
+        this.n = n;
+        this.m = m;
+        validateInputRobotCount(n, m, robotCount);
+        this.robotCount = robotCount;
+        this.robots = new ArrayList<>();
+    }
+
+    /*
+     * Перегрузка конструктора
+     */
+    public RobotMap(int n, int m) {
+        this(n, m, 5);
+    }
+
+    /*
+     * Валидация размерности карты
+     */
+    private void validateRobotMapBoundaries(int n, int m) {
+        if (n <= 0 || m <=0) {
+            throw new IllegalStateException("Некорректная размерность карты!");
+        }
+    }
+
+    /*
+     * Валидация значения количества роботов на карте
+     */
+    private void validateInputRobotCount(int n, int m, int count) {
+        if (count <= 0) {
+            throw new IllegalStateException("Количество роботов не может быть отрицательным или равно нулю");
+        }
+        if (count > m * n) {
+            throw new IllegalStateException("Число роботов не может превышать количества ячеек карты:\n " + "максимальное количество роботов: " +
+            (n * m) + ", ваше число роботов: " + count);
+        }
+    }
+
+    /*
+     * Создание экземпляра робота
+     */
+    public Robot createRobot(Point point) {
+        validatePoint(point);
+        validateCreateRobotCount(robots, robotCount);
+        Robot robot = new Robot(point);
+        robots.add(robot);
+        return robot;
+    }
+
+    /*
+     * Валидация количества роботов при создании экземпляров
+     */
+    private void validateCreateRobotCount(List<Robot> robots, int count) {
+        if (robots.size() >= count) {
+            throw new IllegalStateException(
+                "Нельзя добавить еще одного робота!\n" + 
+                "Данный робот будет " + 
+                (robots.size() + 1) + " из допустимых " +
+                robotCount
+                );
+        }
+    }
+
+    /*
+     * Валидация точки
+     */
+    private void validatePoint(Point point) {
+        validatePointIsCorrect(point);
+        validatePointIsFree(point);
+    }
+
+    /*
+     * Валидация на корректные координаты точки
+     */
+    private void validatePointIsCorrect(Point point) {
+        if (point.x() < 0 || point.x() > n || point.y() < 0 || point.y() > m) {
+            throw new IllegalStateException("Некорректное значение точки!\n Точка находится за пределами игрового поля");
+        }
+    }
+
+    /*
+     * Валидация на незанятую точку
+     */
+    private void validatePointIsFree(Point point) {
+        for (Robot robot : robots) {
+            if (robot.point.equals(point)) {
+                throw new IllegalStateException("Точка " + point + " занята!");
+            }
+        }
+    }
+
+    public Optional<Robot> getByID(Long id) {
+        for (Robot robot : robots) {
+            if (id.equals(robot.id)) {
+                return Optional.of(robot);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public void acceptRobots(Consumer<Robot> robotAcceptor) {
+        for (Robot robot : robots) {
+            robotAcceptor.accept(robot);
+        }
+    }
+
+    public void deleteRobot(Robot robot) {
+        robots.remove(robot);
+        System.out.println("Робот " + robot.toString() + " удален!");
+    }
+
+    /*
+     * Вложенный класс Robot
+     */
+    public class Robot {
+        public static final Direction DEFAUL_DIRECTION = Direction.TOP;
+        private Direction direction;
+        private Point point;
+        private static Long idCounter = 1L;
+        private final Long id;
+
+        /*
+         * Конструктор класса Robot
+         */
+        public Robot(Point point) {
+            this.id = idCounter++;
+            this.direction = DEFAUL_DIRECTION;
+            this.point = point;
+        }
+
+        /*
+         * Метод смены направления движения
+         */
+        public void changeDirection(Direction direction) {
+            Direction prevDir = this.direction;
+            this.direction =  direction;
+            System.out.println("Направление робота [" + id + "] " 
+                                + point.toString() + " ["+ prevDir 
+                                + "]"+ " сменилось на " 
+                                + " [" + direction.name() + "]");
+        }
+
+        /*
+         * Метод движения робота
+         */
+        public void move() {
+            move(1);
+        }
+
+        /*
+         * Перегрузка метода движения робота
+         */
+        public void move(int step) {
+            System.out.println("Количество шагов: " + step );
+            for (int i = 0; i < step; i++) {
+                final Point newPoint;
+                switch(direction) {
+                    case TOP: newPoint = new Point(point.x() - 1, point.y());
+                              break;
+                    case RIGHT: newPoint = new Point(point.x(), point.y() + 1);
+                              break;
+                    case BOTTOM: newPoint = new Point(point.x() + 1, point.y());
+                              break;
+                    case LEFT: newPoint = new Point(point.x(), point.y() - 1);
+                              break;
+                    default: newPoint = new Point(point.x(), point.y());
+                }
+                validatePoint(newPoint);
+                System.out.println("Робот переместился с " + point + " на " + newPoint);
+                this.point = newPoint;
+            }
+        }
+
+        /*
+         * Переобределение метода toString
+         */
+        @Override
+        public String toString() {
+            return "[" + id + "] " + point.toString() + " ["+ direction.name() + "]";
+        }
+    }
+}
